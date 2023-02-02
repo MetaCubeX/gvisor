9b7164de61dfd131307d601bdf7b7f2c5f2f2c1a
wwqgtxx <wwqgtxx@gmail.com>
2023-02-02 15:36:21 +0800

    avoid "unsafe.StringData requires go1.20 or later (-lang was set to go1.18; check go.mod)"

--- pkg/gohacks/string_go113_unsafe.go
+++ pkg/gohacks/string_go113_unsafe.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build go1.13 && !go1.20
-// +build go1.13,!go1.20
+//go:build go1.13 && !go1.21
+// +build go1.13,!go1.21
 
 // TODO(go.dev/issue/8422): Remove this file once Go 1.19 is no longer
 // supported.
